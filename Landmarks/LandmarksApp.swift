2d117186c3953b61c9ad8fe55223f1cb03c92821
Jad Beydoun <33669819+jadb18@users.noreply.github.com>
2023-05-03 03:45:42 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jad Beydoun on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
