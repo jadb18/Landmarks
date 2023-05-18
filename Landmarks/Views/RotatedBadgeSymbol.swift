edcfe62b6011c72ac02c27771a428ddc60863c45
Jad Beydoun <33669819+jadb18@users.noreply.github.com>
2023-05-17 22:56:07 -0400

    Added icon and badge symbol - Not working with Xcode Previews

---
+++ Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Jad Beydoun on 5/17/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
