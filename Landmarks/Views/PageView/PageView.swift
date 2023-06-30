90b48979938e15b0b27c490786d50488de332bb9
Jad Beydoun <33669819+jadb18@users.noreply.github.com>
2023-06-30 13:46:00 -0400

    Additional files for Interfacing with UIKit

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -7,12 +7,17 @@
 
 import SwiftUI
 
-struct PageView: View {
+struct PageView<Page: View>: View {
+    var pages: [Page]
+                
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        PageViewController(pages: pages)
     }
 }
 
-#Preview {
-    PageView()
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
 }
