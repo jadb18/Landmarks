ab187220d4520afff66dd9db485b14b6653c9687
Jad Beydoun <33669819+jadb18@users.noreply.github.com>
2023-06-30 13:43:50 -0400

    Create the View Controller's Data Source

---
+++ Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,18 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Jad Beydoun on 6/30/23.
+//
+
+import SwiftUI
+
+struct PageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageView()
+}
