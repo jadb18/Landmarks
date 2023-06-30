58d15bf4f7bea854ac35938b26a9b4acb123de1b
Jad Beydoun <33669819+jadb18@users.noreply.github.com>
2023-06-30 15:13:25 -0400

    Interfacing with UIKit

---
+++ Landmarks/Views/PageView/PageControl.swift
@@ -0,0 +1,18 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Jad Beydoun on 6/30/23.
+//
+
+import SwiftUI
+
+struct PageControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageControl()
+}
