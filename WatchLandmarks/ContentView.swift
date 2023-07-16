f8c64882ab9fedbf619f0250da24a58abfa345bb
Jad Beydoun <33669819+jadb18@users.noreply.github.com>
2023-07-16 02:56:29 -0400

    Created watchOS app, notification payload not tested

--- WatchLandmarks/ContentView.swift
+++ WatchLandmarks/ContentView.swift
@@ -9,18 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        LandmarkList()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
